040fac06281bcf36a941555db229fd658b3f8b70
Joao Lobo <joao.lobom@gmail.com>
2022-10-09 13:48:09 +0100

    Initialize pubsub class and topic

--- lib/pubsub.rb
+++ lib/pubsub.rb
@@ -3,20 +3,19 @@
 require("google/cloud/pubsub")
 
 class Pubsub
-  # Find or create a topic.
+  # @return [Google::Cloud::PubSub::Project]
+  def initialize
+    @topic_name = "default"
+    @subscription_name = "default"
+    @client = Google::Cloud::PubSub.new(project_id: "code-challenge")
+  end
+
+    # Find or create a topic.
   #
   # @param topic [String] The name of the topic to find or create
   # @return [Google::Cloud::PubSub::Topic]
-  def topic(name)
-    client.topic(name) || client.create_topic(name)
+  def find_or_create_topic(topic_name: @topic_name)
+    @client.topic(topic_name) || @client.create_topic(topic_name)
   end
 
-  private
-
-  # Create a new client.
-  #
-  # @return [Google::Cloud::PubSub]
-  def client
-    @client ||= Google::Cloud::PubSub.new(project_id: "code-challenge")
-  end
 end
