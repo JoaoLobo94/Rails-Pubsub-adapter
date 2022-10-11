f38484dc9d839a7f7e022351c268e58fab6059ca
Joao Lobo <joao.lobom@gmail.com>
2022-10-11 20:01:09 +0100

    Refactor of Pubsub class. no longer initialize method with defaults

--- lib/pubsub.rb
+++ lib/pubsub.rb
@@ -3,27 +3,20 @@
 require("google/cloud/pubsub")
 
 class Pubsub
-  # @return [Google::Cloud::PubSub::Project]
-  def initialize
-    @topic_name = "default"
-    @subscription_name = "default"
-    @client = Google::Cloud::PubSub.new(project_id: "code-challenge")
-  end
-
   # Find or create a topic.
   #
   # @param topic [String] The name of the topic to find or create
   # @return [Google::Cloud::PubSub::Topic]
-  def find_or_create_topic(topic_name: @topic_name)
-    @client.topic(topic_name) || @client.create_topic(topic_name)
+  def find_or_create_topic(topic_name:)
+    client.topic(topic_name) || client.create_topic(topic_name)
   end
 
   # Find or create a subscription.
   #
   # @param subscription_name and name [String] The name of the subscription to find or create
   # @return [Google::Cloud::PubSub::Subscription]
-  def find_or_create_subscription(topic_name: @topic_name, subscription_name: @subscription_name)
-    @client.subscription(subscription_name) || find_or_create_topic(topic_name: topic_name)
+  def find_or_create_subscription(topic_name:, subscription_name:)
+    client.subscription(subscription_name) || find_or_create_topic(topic_name: topic_name)
       .create_subscription(subscription_name)
   end
 
@@ -33,23 +26,24 @@ class Pubsub
   # @param message [String] The message data to publish
   # @param ordering_key [Integer] The ordering key to use for message ordering
 
-  def publish_message(message:, topic_name: @topic_name, ordering_key: 1)
+  def publish_message(message:, topic_name:, ordering_key:)
     topic = find_or_create_topic(topic_name: topic_name)
     topic.enable_message_ordering!
-    topic.publish_async(message, ordering_key: ordering_key)
+    topic.publish(message, ordering_key: ordering_key)
   end
 
   # Listen for messages on a subscription.
   # @param subscription_name [String] The name of the subscription to listen to
   # @return [Google::Cloud::PubSub::ReceivedMessage]
-  def receive_and_perform_jobs(topic_name: @topic_name, subscription_name: @subscription_name)
+  def receive_and_perform_jobs(topic_name: "default", subscription_name: "default")
     subscription = find_or_create_subscription(topic_name: topic_name, subscription_name: subscription_name)
     subscription.message_ordering?
     subscriber = subscription.listen do |received_message|
-      print_job_result(received_message)
+      execute_and_acknowledge_job(received_message)
     end
-    print_exceptions(subscriber)
     at_exit do
+      # print exceptions raised by the subscriber thread on exit
+      print_exceptions(subscriber)
       subscriber.stop!(10)
     end
     subscriber.start
@@ -62,15 +56,20 @@ class Pubsub
   def print_exceptions(subscriber)
     subscriber.on_error do |exception|
       puts("Exception: #{exception.class} #{exception.message}")
-      # acknowledge the message so it won't be redelivered
-      exception.received_message.acknowledge!
     end
   end
   # Print the result of a job.
   # @param received_message [Google::Cloud::PubSub::ReceivedMessage]
 
-  def print_job_result(message)
+  def execute_and_acknowledge_job(message)
     puts("Your calculation result is #{ActiveJob::QueueAdapters::PubsubAdapter::Executor.new(message.data).perform}")
     message.acknowledge!
+  end
+
+    # Create a new client.
+  #
+  # @return [Google::Cloud::PubSub]
+  def client
+    @client ||= Google::Cloud::PubSub.new(project_id: "code-challenge")
   end
 end
