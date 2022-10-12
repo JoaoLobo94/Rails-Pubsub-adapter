08325225862312dfa7ef424e435473aa9b5c04d4
Joao Lobo <joao.lobom@gmail.com>
2022-10-12 20:50:39 +0100

    Refactor adapter so it does not instantiate new pubsub class on every enqueue

--- lib/active_job/queue_adapters/pubsub_adapter.rb
+++ lib/active_job/queue_adapters/pubsub_adapter.rb
@@ -4,35 +4,26 @@ require("securerandom")
 module ActiveJob
   module QueueAdapters
     class PubsubAdapter
-      # Enqueue a job to be performed.
-      # @param [ActiveJob::Base] job The job to be performed.
+      # Initialize pubsub client
+      # @return [Pubsub]
+      def initialize
+        @pubsub = Pubsub.new
+      end
 
+      # Enqueue a job to be performed later.
+      # @param job [ActiveJob::Base] The job to be enqueued
       def enqueue(job)
-        Scheduler.new.enqueue(JobWrapper.new(job), queue_name: job.queue_name, ordering_key: job.priority)
+        @pubsub.publish_message(topic_name: job.queue_name,
+                                message: JobWrapper.new(job).to_json,
+                                ordering_key: job.priority || "1")
       end
 
       # JobWrapper is a wrapper class for serializing jobs to be sent to pubsub
       # Returns a hash of the job's attributes
-      # @return [Hash] The job's attributes
       class JobWrapper
         def initialize(job)
           job.provider_job_id = SecureRandom.uuid
           @job_data = job.serialize
-        end
-      end
-
-      # Scheduler class is responsible for scheduling and publishing jobs to be performed
-      # @param [ActiveJob::Base] job The job to be performed.
-      # @param [String] queue_name The name of the queue to publish to
-      # @param [String] ordering_key The ordering key to use for message ordering
-      class Scheduler
-        def initialize
-          @pubsub = Pubsub.new
-        end
-
-        # Enqueue a job to be performed.
-        def enqueue(job, queue_name:, ordering_key:)
-          @pubsub.publish_message(topic_name: queue_name, message: job.to_json, ordering_key: ordering_key || "1")
         end
       end
 
