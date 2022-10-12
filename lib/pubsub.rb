c04a39f0339521e38f01ea66b2eae9607d712fa3
Joao Lobo <joao.lobom@gmail.com>
2022-10-12 20:51:10 +0100

    Memoise values from previous calls so that we don't reach quota

--- lib/pubsub.rb
+++ lib/pubsub.rb
@@ -3,29 +3,11 @@
 require("google/cloud/pubsub")
 
 class Pubsub
-  # Find or create a topic.
-  #
-  # @param topic [String] The name of the topic to find or create
-  # @return [Google::Cloud::PubSub::Topic]
-  def find_or_create_topic(topic_name:)
-    client.topic(topic_name) || client.create_topic(topic_name)
-  end
-
-  # Find or create a subscription.
-  #
-  # @param subscription_name and name [String] The name of the subscription to find or create
-  # @return [Google::Cloud::PubSub::Subscription]
-  def find_or_create_subscription(topic_name:, subscription_name:)
-    client.subscription(subscription_name) || find_or_create_topic(topic_name: topic_name)
-      .create_subscription(subscription_name)
-  end
-
   # Publish a message to a topic.
 
   # @param topic_name [String] The name of the topic to publish to
   # @param message [String] The message data to publish
   # @param ordering_key [Integer] The ordering key to use for message ordering
-
   def publish_message(message:, topic_name:, ordering_key:)
     topic = find_or_create_topic(topic_name: topic_name)
     topic.enable_message_ordering!
@@ -58,12 +40,23 @@ class Pubsub
       puts("Exception: #{exception.class} #{exception.message}")
     end
   end
+
   # Print the result of a job.
   # @param received_message [Google::Cloud::PubSub::ReceivedMessage]
-
   def execute_and_acknowledge_message(message)
-    puts("Your calculation result is #{ActiveJob::QueueAdapters::PubsubAdapter::Executor.new(message.data).perform}")
+    ActiveJob::QueueAdapters::PubsubAdapter::Executor.new(message.data).perform
     message.acknowledge!
+  end
+
+  # Method detects if user input has changed the topic,
+  # so message is not published to wrong topic
+  # @param current_topic_name [String]
+  # @param incoming_topic_name [String]
+  # @return [Boolean]
+  def topic_name_changed?(current_topic_name, incoming_topic_name)
+    return false if current_topic_name == "projects/code-challenge/topics/#{incoming_topic_name}"
+
+    true
   end
 
   # Create a new client.
@@ -71,5 +64,27 @@ class Pubsub
   # @return [Google::Cloud::PubSub]
   def client
     @client ||= Google::Cloud::PubSub.new(project_id: "code-challenge")
+  end
+
+  # Find or create a topic.
+  #
+  # @param topic [String] The name of the topic to find or create
+  # Check if topic name changed, and if it did, then reassign instance variable
+  # @return [Google::Cloud::PubSub::Topic]
+  def find_or_create_topic(topic_name:)
+    if @find_or_create_topic && topic_name_changed?(@find_or_create_topic&.name, topic_name)
+      @find_or_create_topic = client.topic(topic_name) || client.create_topic(topic_name)
+    else
+      @find_or_create_topic ||= client.topic(topic_name) || client.create_topic(topic_name)
+    end
+  end
+
+  # Find or create a subscription.
+  #
+  # @param subscription_name and name [String] The name of the subscription to find or create
+  # @return [Google::Cloud::PubSub::Subscription]
+  def find_or_create_subscription(topic_name:, subscription_name:)
+    @find_or_create_subscription ||= client.subscription(subscription_name) || find_or_create_topic(topic_name: topic_name)
+                                     .create_subscription(subscription_name)
   end
 end
